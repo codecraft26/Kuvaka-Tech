da90344fb2e31bebcc0e43b8697bfb9cfe747e69
codecraft26 <gaman0221@gmail.com>
2024-03-14 23:43:23 +0530

    Update chat container styling and fix user count display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,50 @@
-import React, { useState, useEffect, useRef } from 'react';
-import io from 'socket.io-client';
-import toastr from 'toastr';
-import 'toastr/build/toastr.min.css';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import io from "socket.io-client";
+import toastr from "toastr";
+import "toastr/build/toastr.min.css";
+import "./App.css";
 
 const App = () => {
   const [messages, setMessages] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
   const [userCount, setUserCount] = useState(0);
 
   const chatContainerRef = useRef(null);
   const socket = useRef(null);
 
   useEffect(() => {
-    socket.current = io('http://localhost:4000', { transports: ['websocket'] });
+    socket.current = io("http://localhost:4000", { transports: ["websocket"] });
 
-    socket.current.on('message', data => {
-      setMessages(prevMessages => [...prevMessages, { message: data.message, sender: data.sender }]);
+    socket.current.on("message", (data) => {
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { message: data.message, sender: data.sender },
+      ]);
       scrollToBottom();
     });
 
-    socket.current.on('userCount', count => {
+    socket.current.on("userCount", (count) => {
       setUserCount(count);
     });
 
-    socket.current.on('newUserConnected', (newUserId) => {
+    socket.current.on("newUserConnected", (newUserId) => {
       toastr.options = {
         closeButton: true,
         timeOut: 5000,
-        positionClass: 'toast-bottom-right'
+        positionClass: "toast-bottom-right",
       };
       toastr.info(`${newUserId} has joined the chat.`);
-      setUserCount(prevCount => prevCount + 1);
+      setUserCount((prevCount) => prevCount + 1);
     });
 
-    socket.current.on('userLeft', (disconnectedSocketId) => {
+    socket.current.on("userLeft", (disconnectedSocketId) => {
       toastr.options = {
         closeButton: true,
         timeOut: 5000,
-        positionClass: 'toast-bottom-right toast-middle'
+        positionClass: "toast-bottom-right toast-middle",
       };
       toastr.info(`${disconnectedSocketId} has left the chat.`);
-      setUserCount(prevCount => Math.max(0, prevCount - 1));
+      setUserCount((prevCount) => Math.max(0, prevCount - 1));
     });
 
     return () => {
@@ -51,15 +54,18 @@ const App = () => {
     };
   }, []);
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (inputValue.trim() !== '') {
+    if (inputValue.trim() !== "") {
       if (socket.current) {
-        socket.current.emit('message', { message: inputValue });
-        setMessages(prevMessages => [...prevMessages, { message: inputValue, sender: 'You' }]);
+        socket.current.emit("message", { message: inputValue });
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          { message: inputValue, sender: "You" },
+        ]);
         scrollToBottom();
       }
-      setInputValue('');
+      setInputValue("");
     }
   };
 
@@ -70,23 +76,32 @@ const App = () => {
   return (
     <div className="container">
       <div id="chat-container" ref={chatContainerRef}>
-        <ul id="messages" className="messages">
-          {messages.map((msg, index) => (
-            <li key={index} className={`message ${msg.sender === 'You' ? 'own-message' : 'other-message'}`}>
-              <div className="message-content">{msg.message}</div>
-              {msg.sender !== 'You' && <span className="sender"> - {msg.sender}</span>}
-            </li>
-          ))}
-        </ul>
+        {/* <h3 id="user-count">Users Online: {userCount}</h3> */}
+        <div className="chat-list-container">
+          <ul id="messages" className="messages">
+            {messages.map((msg, index) => (
+              <li
+                key={index}
+                className={`message ${
+                  msg.sender === "You" ? "own-message" : "other-message"
+                }`}
+              >
+                <div className="message-content">{msg.message}</div>
+                {msg.sender !== "You" && (
+                  <span className="sender"> - {msg.sender}</span>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div id="user-count">Users Online: {userCount}</div>
       <form id="form" onSubmit={handleSubmit}>
         <input
           id="input"
           autoComplete="off"
           placeholder="Type a message..."
           value={inputValue}
-          onChange={e => setInputValue(e.target.value)}
+          onChange={(e) => setInputValue(e.target.value)}
         />
         <button type="submit">Send</button>
       </form>
